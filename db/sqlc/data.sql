12b8e4c410858211b6a82f4998290cd7b591c6ed
ShadrackAdwera <adweshshaddie@gmail.com>
2023-02-26 17:23:45 +0300

    write CRUD sql statements for data operations

--- db/sqlc/data.sql
+++ db/sqlc/data.sql
@@ -20,3 +20,29 @@ RETURNING *;
 SELECT * 
 FROM data
 WHERE created_by_id = $1;
+
+-- name: GetDataById :one
+SELECT * 
+FROM data
+WHERE id = $1;
+
+-- name: UpdateData :one
+UPDATE data 
+SET
+  home_scored = COALESCE(sqlc.narg(home_scored),home_scored),
+  away_scored = COALESCE(sqlc.narg(away_scored),away_scored),
+  home_team = COALESCE(sqlc.narg(home_team),home_team),
+  away_team = COALESCE(sqlc.narg(away_team),away_team)
+  match_day = COALESCE(sqlc.narg(match_day),match_day),
+  referee = COALESCE(sqlc.narg(referee),referee),
+  is_blocked = COALESCE(sqlc.narg(is_blocked),is_blocked),
+  winner = COALESCE(sqlc.narg(winner),winner)
+  season = COALESCE(sqlc.narg(season),season),
+  file_id = COALESCE(sqlc.narg(file_id),file_id)
+WHERE id = sqlc.arg(id)
+RETURNING *;
+
+-- name: DeleteData :exec
+DELETE FROM 
+data 
+WHERE id = $1;
