93774b0ac1f1a9bbd7e0af74e2cf2f51038e7590
ShadrackAdwera <adweshshaddie@gmail.com>
2023-02-26 17:12:14 +0300

    modify init schema

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "users" (
   "id" bigserial PRIMARY KEY,
   "username" varchar UNIQUE NOT NULL,
-  "password" varchar NOT NULL,
   "email" varchar UNIQUE NOT NULL,
+  "password" varchar NOT NULL,
   "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
