5506155083b517f16d9c13eeee01838b6d21b7c3
ShadrackAdwera <adweshshaddie@gmail.com>
2023-02-28 11:20:27 +0300

    create transaction util fn

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -1,6 +1,9 @@
 package db
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+)
 
 // code for transactions go here
 type TxStore interface {
@@ -17,4 +20,25 @@ func NewStore(db *sql.DB) TxStore {
 		db:      db,
 		Queries: New(db),
 	}
+}
+
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, &sql.TxOptions{
+		Isolation: sql.LevelReadCommitted,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+
+	if err != nil {
+		if txErr := tx.Rollback(); txErr != nil {
+			return txErr
+		}
+		return err
+	}
+	return tx.Commit()
 }
