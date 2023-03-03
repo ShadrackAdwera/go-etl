afd106d9d12b3fcdc6584ac488535528e942de3d
ShadrackAdwera <adweshshaddie@gmail.com>
2023-03-03 17:41:14 +0300

    pass user id as uri params

--- api/file-handlers.go
+++ api/file-handlers.go
@@ -6,9 +6,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type UserArgs struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
 func (srv *Server) getFiles(ctx *gin.Context) {
 	// TODO: Implement middleware
-	files, err := srv.store.GetFiles(ctx, 1)
+
+	var userId UserArgs
+
+	if err := ctx.ShouldBindUri(&userId); err != nil {
+		ctx.JSON(http.StatusBadRequest, errJSON(err))
+		return
+	}
+
+	files, err := srv.store.GetFiles(ctx, userId.ID)
 
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errJSON(err))
