3a578aa6cc88c76d5389cc392caeaf3dd6317f4a
ShadrackAdwera <adweshshaddie@gmail.com>
2023-03-09 17:32:19 +0300

    add auth to get files endpoint

--- api/file-handlers_test.go
+++ api/file-handlers_test.go
@@ -12,10 +12,20 @@ import (
 
 	mockdb "github.com/ShadrackAdwera/go-etl/db/mocks"
 	db "github.com/ShadrackAdwera/go-etl/db/sqlc"
+	"github.com/ShadrackAdwera/go-etl/token"
 	"github.com/ShadrackAdwera/go-etl/utils"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 )
+
+// func setUpAuthToken(t *testing.T, request *http.Request, tokenMaker token.TokenMaker, username string, id int64, email string, duration time.Duration) {
+// 	payload, tkn, err := tokenMaker.CreateToken(username, id, email, duration)
+// 	require.NoError(t, err)
+// 	require.NotEmpty(t, payload)
+
+// 	bearerToken := fmt.Sprintf("%s %s", authType, tkn)
+// 	request.Header.Set(authorization, bearerToken)
+// }
 
 func TestGetFiles(t *testing.T) {
 	file := GenerateRandomFile()
@@ -23,12 +33,18 @@ func TestGetFiles(t *testing.T) {
 	testCases := []struct {
 		name       string
 		file       db.File
+		setUpAuth  func(t *testing.T, request *http.Request, tokenMaker token.TokenMaker)
 		buildStubs func(store *mockdb.MockTxStore)
 		comparator func(t *testing.T, recorder *httptest.ResponseRecorder)
 	}{
 		{
 			name: "Status OK",
 			file: file,
+			setUpAuth: func(t *testing.T, request *http.Request, tokenMaker token.TokenMaker) {
+				username := utils.RandomString(6)
+				email := fmt.Sprintf("%s@mail.com", username)
+				setUpToken(t, request, tokenMaker, authorization, authType, username, file.CreatedByID, email, time.Minute)
+			},
 			buildStubs: func(store *mockdb.MockTxStore) {
 				store.EXPECT().GetFiles(gomock.Any(), gomock.Eq(file.CreatedByID)).Times(1).Return([]db.File{
 					file,
@@ -55,11 +71,11 @@ func TestGetFiles(t *testing.T) {
 
 			recorder := httptest.NewRecorder()
 
-			url := fmt.Sprintf("/api/files/%v", testCase.file.CreatedByID)
-
-			request, err := http.NewRequest(http.MethodGet, url, nil)
+			request, err := http.NewRequest(http.MethodGet, "/api/files", nil)
 
 			require.NoError(t, err)
+
+			testCase.setUpAuth(t, request, server.tokenMaker)
 
 			server.router.ServeHTTP(recorder, request)
 			testCase.comparator(t, recorder)
