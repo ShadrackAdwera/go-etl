439dbf70269f5397c95b1b51e06128fc764a0b7f
ShadrackAdwera <adweshshaddie@gmail.com>
2023-03-10 08:24:17 +0300

    create refresh token auth handler

--- api/auth-handlers.go
+++ api/auth-handlers.go
@@ -2,12 +2,16 @@ package api
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"net/http"
 	"time"
 
 	db "github.com/ShadrackAdwera/go-etl/db/sqlc"
+	"github.com/ShadrackAdwera/go-etl/token"
 	"github.com/ShadrackAdwera/go-etl/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type User struct {
@@ -17,10 +21,12 @@ type User struct {
 }
 
 type AuthResponse struct {
-	User                 User      `json:"user"`
-	AccessToken          string    `json:"access_token"`
-	RefreshToken         string    `json:"refresh_token"`
-	AccessTokenExpiresAt time.Time `json:"access_token_expires_at"`
+	User                  User      `json:"user"`
+	AccessToken           string    `json:"access_token"`
+	RefreshToken          string    `json:"refresh_token"`
+	AccessTokenExpiresAt  time.Time `json:"access_token_expires_at"`
+	RefreshTokenExpiresAt time.Time `json:"refresh_token_expires_at"`
+	SessionID             uuid.UUID `json:"session_id"`
 }
 
 type SignUpRequest struct {
@@ -55,32 +61,11 @@ func (srv *Server) signUp(ctx *gin.Context) {
 		return
 	}
 
-	aPayload, accessTkn, err := srv.tokenMaker.CreateToken(user.Username, user.ID, user.Email, time.Minute*15)
-
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errJSON(err))
-		return
-	}
-
-	_, refreshTkn, err := srv.tokenMaker.CreateToken(user.Username, user.ID, user.Email, time.Hour*24)
-
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errJSON(err))
-		return
-	}
-
-	resCreateUser := AuthResponse{
-		User: User{
-			ID:       user.ID,
-			Username: user.Username,
-			Email:    user.Email,
-		},
-		AccessToken:          accessTkn,
-		RefreshToken:         refreshTkn,
-		AccessTokenExpiresAt: aPayload.ExpiredAt,
-	}
-
-	ctx.JSON(http.StatusCreated, resCreateUser)
+	ctx.JSON(http.StatusCreated, User{
+		ID:       user.ID,
+		Username: user.Username,
+		Email:    user.Email,
+	})
 }
 
 type LoginRequest struct {
@@ -120,22 +105,102 @@ func (srv *Server) login(ctx *gin.Context) {
 		return
 	}
 
-	_, refreshTkn, err := srv.tokenMaker.CreateToken(user.Username, user.ID, user.Email, time.Hour*24)
+	rPayload, refreshTkn, err := srv.tokenMaker.CreateToken(user.Username, user.ID, user.Email, time.Hour*24)
 
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errJSON(err))
 		return
 	}
+
+	session, err := srv.store.CreateSession(ctx, db.CreateSessionParams{
+		ID:           rPayload.TokenId,
+		Username:     rPayload.Username,
+		UserID:       rPayload.ID,
+		RefreshToken: refreshTkn,
+		UserAgent:    ctx.Request.UserAgent(),
+		ClientIp:     ctx.ClientIP(),
+		IsBlocked:    false,
+		ExpiresAt:    rPayload.ExpiredAt,
+	})
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errJSON(err))
+		return
+	}
+
 	loginResponse := AuthResponse{
 		User: User{
 			ID:       user.ID,
 			Username: user.Username,
 			Email:    user.Email,
 		},
-		AccessToken:          accessTkn,
-		RefreshToken:         refreshTkn,
-		AccessTokenExpiresAt: aPayload.ExpiredAt,
+		AccessToken:           accessTkn,
+		RefreshToken:          refreshTkn,
+		AccessTokenExpiresAt:  aPayload.ExpiredAt,
+		RefreshTokenExpiresAt: rPayload.ExpiredAt,
+		SessionID:             session.ID,
 	}
 
 	ctx.JSON(http.StatusOK, loginResponse)
+}
+
+type RefreshTokenArgs struct {
+	RefreshToken string `json:"refresh_token" binding:"required"`
+}
+
+func (srv *Server) renewAccessToken(ctx *gin.Context) {
+	var refreshTokenArgs RefreshTokenArgs
+
+	if err := ctx.ShouldBindJSON(&refreshTokenArgs); err != nil {
+		ctx.JSON(http.StatusBadRequest, errJSON(errors.New("invalid request")))
+		return
+	}
+
+	aTknPayload := ctx.MustGet(authPayload).(*token.TokenPayload)
+
+	rTknPayload, err := srv.tokenMaker.VerifyToken(refreshTokenArgs.RefreshToken)
+
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, errJSON(fmt.Errorf("%s. login to create a new session", err)))
+		return
+	}
+
+	if aTknPayload.ID != rTknPayload.ID {
+		ctx.JSON(http.StatusUnauthorized, errJSON(fmt.Errorf("you are not authorized to make this request")))
+		return
+	}
+
+	foundSess, err := srv.store.GetSession(ctx, rTknPayload.TokenId)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errJSON(fmt.Errorf("this session is not valid")))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errJSON(err))
+		return
+	}
+
+	if foundSess.IsBlocked {
+		ctx.JSON(http.StatusUnauthorized, errJSON(fmt.Errorf("your account has been blocc'd")))
+		return
+	}
+
+	if foundSess.ID != rTknPayload.TokenId {
+		ctx.JSON(http.StatusUnauthorized, errJSON(fmt.Errorf("your account has been blocc'd")))
+		return
+	}
+
+	accessTknPayload, tkn, err := srv.tokenMaker.CreateToken(aTknPayload.Username, aTknPayload.ID, aTknPayload.Email, time.Minute*15)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errJSON(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"access_token":        tkn,
+		"access_token_expiry": accessTknPayload.ExpiredAt,
+	})
+
 }
--- api/server.go
+++ api/server.go
@@ -35,6 +35,7 @@ func NewServer(store db.TxStore, config utils.AppConfig) *Server {
 
 	// auth middleware
 	authRoutes := router.Group("/").Use(authMiddleware(tokenMaker))
+	authRoutes.POST("/api/renew-access-token", server.renewAccessToken)
 	authRoutes.GET("/api/files", server.getFiles)
 	authRoutes.POST("/api/files", server.uploadCsvFile)
 	authRoutes.GET("/api/matches", server.getMatches)
@@ -48,5 +49,5 @@ func (s *Server) StartServer(serverAddress string) error {
 }
 
 func errJSON(err error) gin.H {
-	return gin.H{"message": fmt.Sprintf("error occured: %v", err)}
+	return gin.H{"message": fmt.Sprintf("%v", err)}
 }
